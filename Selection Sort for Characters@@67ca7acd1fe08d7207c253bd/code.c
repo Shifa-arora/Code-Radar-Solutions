1993cd3214eb5825efef6affdcc453cf00a4e64f
Shifa <shifachugh06@gmail.com>
2025-03-31 13:49:40 +0530

    Solution to Selection Sort for Characters

--- Selection Sort for Characters@@67ca7acd1fe08d7207c253bd/code.c
+++ Selection Sort for Characters@@67ca7acd1fe08d7207c253bd/code.c
@@ -1,31 +1,30 @@
-// Your code here...#include <stdio.h>
+#include <stdio.h>
 
-// Function to perform Selection Sort
+// Function to perform Selection Sort for characters
 void selectionSort(char arr[], int n) {
     for (int i = 0; i < n - 1; i++) {
         int minIndex = i;
 
-        // Find the minimum element in the remaining array
+        // Find the minimum character in the remaining array
         for (int j = i + 1; j < n; j++) {
             if (arr[j] < arr[minIndex]) {
                 minIndex = j;
             }
         }
 
-        // Swap the found minimum element with the first element
+        // Swap the found minimum character with the first character
         if (minIndex != i) {
-            int temp = arr[i];
+            char temp = arr[i];
             arr[i] = arr[minIndex];
             arr[minIndex] = temp;
         }
     }
 }
 
-// Function to print the array
-void printArray(int arr[], int n) {
+// Function to print the character array
+void printArray(char arr[], int n) {
     for (int i = 0; i < n; i++) {
-        printf("%d ", arr[i]);
+        printf("%c ", arr[i]);
     }
     printf("\n");
 }
-
