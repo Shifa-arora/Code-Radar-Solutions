4abf6582685b460176fd6de4c80e7466d4f53617
Shifa <shifachugh06@gmail.com>
2025-04-08 19:19:57 +0530

    Solution to Find the Duplicate Element in an Array

--- Find the Duplicate Element in an Array@@676c02002dbc8c3e7a280b7b/code.c
+++ Find the Duplicate Element in an Array@@676c02002dbc8c3e7a280b7b/code.c
@@ -6,32 +6,24 @@ int main() {
     scanf("%d", &n);
 
     int* arr = (int*)malloc(n * sizeof(int));
-    int* leaders = (int*)malloc(n * sizeof(int));
-    int count = 0;
-
     for (int i = 0; i < n; i++) {
         scanf("%d", &arr[i]);
     }
 
-    int maxRight = -1;
-    for (int i = n - 1; i >= 0; i--) {
-        if (arr[i] > maxRight) {
+    int maxRight = arr[n - 1];
+    int minLeader = arr[n - 1];
+
+    for (int i = n - 2; i >= 0; i--) {
+        if (arr[i] >= maxRight) {
             maxRight = arr[i];
-            leaders[count++] = arr[i];
+            if (arr[i] < minLeader) {
+                minLeader = arr[i];
+            }
         }
     }
 
-    // Now find the minimum among the leaders
-    int min = leaders[0];
-    for (int i = 1; i < count; i++) {
-        if (leaders[i] < min) {
-            min = leaders[i];
-        }
-    }
-
-    printf("%d\n", min);
+    printf("%d\n", minLeader);
 
     free(arr);
-    free(leaders);
     return 0;
 }
